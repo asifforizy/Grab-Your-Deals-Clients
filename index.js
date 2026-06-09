1738f21e51ad45cc64167843ce71abeec7e7c01e
Asif Forizy <asifforizy@gmail.com>
2026-06-09 19:58:50 +0600

    CRUD oparation added

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 const express = require("express");
-const { MongoClient, ServerApiVersion } = require("mongodb");
-require('dotenv').config()
+const cors = require("cors");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+require("dotenv").config();
 const app = express();
 const port = 3000;
 
+app.use(express.json());
+app.use(cors());
 
-
-const uri =
-  `mongodb://${process.env.DBUSERNAME}:${process.env.DBPASSWORD}@ac-6pk8apo-shard-00-00.hr3iszz.mongodb.net:27017,ac-6pk8apo-shard-00-01.hr3iszz.mongodb.net:27017,ac-6pk8apo-shard-00-02.hr3iszz.mongodb.net:27017/?ssl=true&replicaSet=atlas-bn2j42-shard-0&authSource=admin&appName=Cluster0`;
+const uri = `mongodb://${process.env.DBUSERNAME}:${process.env.DBPASSWORD}@ac-6pk8apo-shard-00-00.hr3iszz.mongodb.net:27017,ac-6pk8apo-shard-00-01.hr3iszz.mongodb.net:27017,ac-6pk8apo-shard-00-02.hr3iszz.mongodb.net:27017/?ssl=true&replicaSet=atlas-bn2j42-shard-0&authSource=admin&appName=Cluster0`;
 
 const client = new MongoClient(uri, {
   serverApi: {
@@ -24,19 +25,50 @@ app.get("/", (req, res) => {
 async function run() {
   try {
     await client.connect();
-    await client.db("admin").command({ ping: 1 });
-    console.log(
-      "Pinged your deployment. You successfully connected to MongoDB!"
-    );
+    const db = client.db("GrabYourDeals");
+    const productsCollection = db.collection("products");
+
+    app.post("/products", async (req, res) => {
+      const newProduct = req.body;
+      const result = await productsCollection.insertOne(newProduct);
+      res.send(result);
+    });
+
+    app.get("/products", async (req, res) => {
+      const cursor = productsCollection.find();
+      const products = await cursor.toArray();
+      res.send(products);
+    });
+
+    app.get("/products/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const product = await productsCollection.findOne(query);
+      res.send(product);
+    });
+
+    app.patch("/products/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const updatedProduct = req.body;
+      const updateDoc = { $set: updatedProduct };
+      const result = await productsCollection.updateOne(query, updateDoc);
+      res.send(result);
+    });
+
+    app.delete("/products/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await productsCollection.deleteOne(query);
+      res.send(result);
+    });
+
+    console.log("Connected to MongoDB");
   } finally {
   }
 }
 run().catch(console.dir);
 
-
-
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
-
-
